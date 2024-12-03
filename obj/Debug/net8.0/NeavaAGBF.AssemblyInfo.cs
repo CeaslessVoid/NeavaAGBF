25f1c611dd639b0f75b0c06ff859a9def3d5c2f7
CeaslessVoid <crocamedelain@gmail.com>
2024-12-03 22:05:54 +1100

    123123

--- obj/Debug/net8.0/NeavaAGBF.AssemblyInfo.cs
+++ obj/Debug/net8.0/NeavaAGBF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeavaAGBF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b4e9a94347724531df618fe4e8b652261b35b95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae96247a28f751b5bca0ad7224be2c0a61ff9e28")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeavaAGBF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeavaAGBF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
