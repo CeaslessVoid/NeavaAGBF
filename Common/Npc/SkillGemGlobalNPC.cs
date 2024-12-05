0a7647eb3ac16e56a65b70ce8bc02f3a4e44f2ec
CeaslessVoid <crocamedelain@gmail.com>
2024-12-05 21:42:48 +1100

    123123

---
+++ Common/Npc/SkillGemGlobalNPC.cs
@@ -0,0 +1,58 @@
+﻿using NeavaAGBF.Content.Items.Others;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace NeavaAGBF.Common.Npc
+{
+    public class SkillGemGlobalNPC : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            if (!npc.friendly && npc.lifeMax > 5 && !npc.boss)
+            {
+                npcLoot.Add(ItemDropRule.ByCondition(new SkillGemDropCondition(), ModContent.ItemType<SkillGem>(), 1, 1, 2));
+            }
+        }
+    }
+
+    public class SkillGemDropCondition : IItemDropRuleCondition
+    {
+
+        public bool CanDrop(DropAttemptInfo info)
+        {
+            if (info.npc.friendly || info.npc.lifeMax <= 5 || info.npc.boss)
+            {
+                return false;
+            }
+
+            if (NPC.downedPlantBoss)
+            {
+                return Main.rand.NextBool(50);
+            }
+            else if (Main.hardMode)
+            {
+                return Main.rand.NextBool(100);
+            }
+            else
+            {
+                return Main.rand.NextBool(200);
+            }
+        }
+
+        public bool CanShowItemDropInUI()
+        {
+            return true;
+        }
+
+        public string GetConditionDescription()
+        {
+            return "Drops Skill Gems";
+        }
+    }
+}
--- obj/Debug/net8.0/NeavaAGBF.AssemblyInfo.cs
+++ obj/Debug/net8.0/NeavaAGBF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeavaAGBF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda08d622f2ae1eea3a19f313f2a46f1f010ab04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37fb61e2e68c75bc885328bacbeb9d7118400b20")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeavaAGBF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeavaAGBF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
