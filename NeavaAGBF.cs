2e613aa29979bbb8f8ea0f826c7b05dec3083153
CeaslessVoid <hpha0051@student.monash.edu>
2025-05-20 14:56:20 +1000

    Reset

--- NeavaAGBF.cs
+++ NeavaAGBF.cs
@@ -1,57 +1,15 @@
-using Terraria;
-using Terraria.ModLoader;
-using Terraria.UI;
-using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
-using Terraria.GameInput;
-using Microsoft.Xna.Framework.Graphics;
-using Terraria.GameContent.UI.Elements;
-using NeavaAGBF.Common.Players;
-using Microsoft.Xna.Framework.Input;
-using NeavaAGBF.Common.UI;
-using NeavaAGBF.Common.Items;
 using System.Linq;
-using Terraria.ID;
-using Terraria.GameContent;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
 
 namespace NeavaAGBF
 {
-    public class NeavaAGBF : Mod
-    {
-        public static ModKeybind ChargeAttackKey;
+	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+	public class NeavaAGBF : Mod
+	{
 
-        public override void Load()
-        {
-            base.Load();
-            On_Main.DrawEmoteBubblesButton += new On_Main.hook_DrawEmoteBubblesButton(this.On_Main_DrawEmoteBubblesButton);
-
-            LoadUncapGroups.Initialize();
-
-            ChargeAttackKey = KeybindLoader.RegisterKeybind(this, "Charge Attack", Keys.O);
-
-            if (!Main.dedServ) // Only load UI on the client
-            {
-                ChargeBar.Instance = new ChargeBar();
-                ChargeBar.Visible = true;
-            }
-
-        }
-
-        public override void Unload()
-        {
-            ChargeBar.Instance = null;
-            ChargeAttackKey = null;
-            LoadUncapGroups.Clear();
-        }
-
-        private void On_Main_DrawEmoteBubblesButton(On_Main.orig_DrawEmoteBubblesButton orig, int pivotTopLeftX, int pivotTopLeftY)
-        {
-            NeavaAGBFPlayer.DrawWeaponGrid();
-            NeavaAGBFPlayer.UpdateIsClicking();
-
-            orig.Invoke(pivotTopLeftX, pivotTopLeftY);
-        }
-
-    }
-
+	}
 }
