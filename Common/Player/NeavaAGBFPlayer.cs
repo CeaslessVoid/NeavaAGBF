cd333e75dcdbd7765c2ac0933f44ad01d94965f1
CeaslessVoid <hpha0051@student.monash.edu>
2025-05-20 15:58:39 +1000

    123123

---
+++ Common/Player/NeavaAGBFPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace NeavaAGBF.Common.Player
+{
+    public class NeavaAGBFPlayer : ModPlayer
+    {
+        public Item[] WeaponGrid = new Item[9];
+
+        public static bool _isMouseOverSlot = false;
+        private static bool _isWeaponGridInitialized = false;
+
+        public static bool IsWeaponGridOpen = false;
+        public static bool IsClicking = false;
+
+
+    }
+}
--- NeavaAGBF.cs
+++ NeavaAGBF.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace NeavaAGBF
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class NeavaAGBF : Mod
-	{
+    // Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+    public class NeavaAGBF : Mod
+    {
+        public override void Load()
+        {
+            On_Main.DrawEmoteBubblesButton += new On_Main.hook_DrawEmoteBubblesButton(this.On_Main_DrawEmoteBubblesButton);
+        }
 
-	}
+        // I don't acutally know how to make a fucking GUI
+        private void On_Main_DrawEmoteBubblesButton(On_Main.orig_DrawEmoteBubblesButton orig, int pivotTopLeftX, int pivotTopLeftY)
+        {
+            //NeavaAGBFPlayer.DrawWeaponGrid();
+            //NeavaAGBFPlayer.UpdateIsClicking();
+
+            orig.Invoke(pivotTopLeftX, pivotTopLeftY);
+        }
+    }
+
 }
